c9e0fb08d69896c93aa03918b78586599fe7b97a
Eric Molitor <eric@molitor.org>
2018-05-05 14:23:19 +0100

    Fix libcxx build and remove unused builds

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -81,6 +81,33 @@ musl() {
 }
 
 
+libcxx() {
+  init 
+
+  mkdir build-stage-libcxx
+  sh -c "cd build-stage-libcxx; cmake \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=$PWD/stage \
+	-DCMAKE_C_COMPILER=clang \
+	-DCMAKE_CXX_COMPILER=clang++ \
+	-DCMAKE_C_FLAGS=\"--sysroot=$PWD/stage --target=$TARGET \" \
+	-DCMAKE_CXX_FLAGS=\"--sysroot=$PWD/stage --target=$TARGET -I$PWD/src/$LLVM/projects/libcxx/include\" \
+	-DLLVM_DEFAULT_TARGET_TRIPLE=$TARGET \
+	-DLLVM_TARGET_ARCH=X86 \
+	-DLLVM_TARGETS_TO_BUILD=X86 \
+	-DLIBCXXABI_USE_LLVM_UNWINDER=True \
+	-DLIBCXX_CXX_ABI=libcxxabi \
+	-DLIBCXX_CXX_ABI_INCLUDE_PATHS=$PWD/src/$LLVM/projects/libcxxabi/include \
+	-DLIBCXX_HAS_MUSL_LIBC=True \
+	-DLIBCXX_HAS_GCC_S_LIB=False \
+	$PWD/src/$LLVM"
+
+  make -j8 -C build-stage-libcxx unwind cxxabi
+  make -C build-stage-libcxx install-unwind install-cxxabi
+  make -j8 -C build-stage-libcxx cxx
+  make -C build-stage-libcxx install-cxx
+}
+
 libxml2() {
   init 
 
@@ -98,150 +125,9 @@ libxml2() {
 }
 
 
-libcxx() {
-  init 
-
-  mkdir build-stage-libcxx
-  sh -c "cd build-stage-libcxx; cmake \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_INSTALL_PREFIX=$PWD/stage \
-	-DCMAKE_C_COMPILER=clang \
-	-DCMAKE_CXX_COMPILER=clang++ \
-	-DCMAKE_C_FLAGS=\"--sysroot=$PWD/stage --target=$TARGET \" \
-	-DCMAKE_CXX_FLAGS=\"--sysroot=$PWD/stage --target=$TARGET -I$PWD/src/$LLVM/projects/libcxx/include\" \
-	-DLIBXML2_INCLUDE_DIR=$PWD/stage/include/libxml2 \
-	-DLIBXML2_LIBRARY=$PWD/stage/usr/libxml2.so \
-	-DLIBXML2_XMLLINT_EXECUTABLE=$PWD/stage/bin/xmllint \
-	-DLLVM_DEFAULT_TARGET_TRIPLE=$TARGET \
-	-DLLVM_TARGET_ARCH=X86 \
-	-DLLVM_TARGETS_TO_BUILD=X86 \
-	-DLLVM_ENABLE_EH=True \
-	-DLLVM_ENABLE_RTTI=True \
-	-DLLVM_ENABLE_LIBXML2=True \
-	-DLLVM_TOOL_LTO_BUILD=False \
-	-DLIBUNWIND_USE_COMPILER_RT=True \
-	-DCOMPILER_RT_BUILD_BUILTINS=True \
-	-DCOMPILER_RT_BUILD_SANITIZERS=False \
-	-DCOMPILER_RT_BUILD_XRAY=False \
-	-DCOMPILER_RT_BUILD_LIBFUZZER=False \
-	-DCOMPILER_RT_BUILD_PROFILE=False \
-	-DLIBCXXABI_USE_COMPILER_RT=True \
-	-DLIBCXXABI_USE_LLVM_UNWINDER=True \
-	-DLIBCXX_CXX_ABI=libcxxabi \
-	-DLIBCXX_CXX_ABI_INCLUDE_PATHS=$PWD/src/$LLVM/projects/libcxxabi/include \
-	-DLIBCXX_CXX_ABI_LIBRARY_PATH=$PWD/build-stage-llvm/lib \
-	-DLIBCXX_HAS_MUSL_LIBC=True \
-	-DLIBCXX_USE_COMPILER_RT=True \
-	$PWD/src/$LLVM"
-
-  make -j8 -C build-stage-libcxx unwind cxxabi cxx
-  make -C build-stage-libcxx install-unwind install-cxxabi install-cxx
-}
-
-llvm() {
-  init
-
-#	-DCMAKE_CXX_FLAGS=\"--sysroot=$PWD/stage --target=$TARGET -D_GLIBCXX_OS_DEFINES=1 \" \
-
-  mkdir build-stage-llvm
-  sh -c "cd build-stage-llvm; cmake -GNinja \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_INSTALL_PREFIX=$PWD/stage \
-	-DCMAKE_C_COMPILER=clang \
-	-DCMAKE_CXX_COMPILER=clang++ \
-	-DCMAKE_C_FLAGS=\"--sysroot=$PWD/stage --target=$TARGET \" \
-	-DCMAKE_CXX_FLAGS=\"--sysroot=$PWD/stage --target=$TARGET -I$PWD/src/$LLVM/projects/libcxx/include\" \
-	-DLIBXML2_INCLUDE_DIR=$PWD/stage/include/libxml2 \
-	-DLIBXML2_LIBRARY=$PWD/stage/usr/libxml2.so \
-	-DLIBXML2_XMLLINT_EXECUTABLE=$PWD/stage/bin/xmllint \
-	-DLLVM_DEFAULT_TARGET_TRIPLE=$TARGET \
-	-DLLVM_TARGET_ARCH=X86 \
-	-DLLVM_TARGETS_TO_BUILD=X86 \
-	-DLLVM_ENABLE_EH=True \
-	-DLLVM_ENABLE_RTTI=True \
-	-DLLVM_ENABLE_LIBXML2=True \
-	-DLLVM_TOOL_LTO_BUILD=False \
-	-DLIBUNWIND_USE_COMPILER_RT=True \
-	-DCOMPILER_RT_BUILD_BUILTINS=True \
-	-DCOMPILER_RT_BUILD_SANITIZERS=False \
-	-DCOMPILER_RT_BUILD_XRAY=False \
-	-DCOMPILER_RT_BUILD_LIBFUZZER=False \
-	-DCOMPILER_RT_BUILD_PROFILE=False \
-	-DLIBCXXABI_USE_COMPILER_RT=True \
-	-DLIBCXXABI_USE_LLVM_UNWINDER=True \
-	-DLIBCXX_CXX_ABI=libcxxabi \
-	-DLIBCXX_CXX_ABI_INCLUDE_PATHS=$PWD/src/$LLVM/projects/libcxxabi/include \
-	-DLIBCXX_CXX_ABI_LIBRARY_PATH=$PWD/build-stage-llvm/lib \
-	-DLIBCXX_HAS_MUSL_LIBC=True \
-	-DLIBCXX_USE_COMPILER_RT=True \
-	$PWD/src/$LLVM"
-}
-
-clang() {
-  init
-
-  mkdir build-host-clang
-  sh -c "cd build-host-clang; cmake \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_INSTALL_PREFIX=$PWD/host \
-	-DCMAKE_C_COMPILER=clang \
-	-DCMAKE_CXX_COMPILER=clang++ \
-	$PWD/src/$LLVM"
-  make -j12  -C build-host-clang llvm-tblgen clang-tblgen
-  mkdir -p $PWD/host/bin
-  cp $PWD/build-host-clang/bin/* $PWD/host/bin
-
-  mkdir build-stage-clang
-  sh -c "cd build-stage-clang; cmake -GNinja \
-	-DCMAKE_CROSSCOMPILING=True \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_INSTALL_PREFIX=$PWD/fakeroot \
-	-DCMAKE_C_COMPILER=clang \
-	-DCMAKE_CXX_COMPILER=clang++ \
-	-DCMAKE_C_FLAGS=\"--sysroot=$PWD/stage --target=$TARGET \" \
-	-DCMAKE_CXX_FLAGS=\"--sysroot=$PWD/stage --target=$TARGET \" \
-        -DCMAKE_EXE_LINKER_FLAGS=-L$PWD/stage/usr/lib \
-        -DCMAKE_SHARED_LINKER_FLAGS=-L$PWD/stage/usr/lib \
-	-DLIBXML2_INCLUDE_DIR=$PWD/stage/usr/include/libxml2 \
-	-DLIBXML2_LIBRARY=$PWD/stage/usr/lib/libxml2.so \
-	-DLIBXML2_XMLLINT_EXECUTABLE=$PWD/stage/usr/bin/xmllint \
-	-DLLVM_DEFAULT_TARGET_TRIPLE=$TARGET \
-	-DLLVM_TARGET_ARCH=X86 \
-	-DLLVM_TARGETS_TO_BUILD=X86 \
-	-DLLVM_ENABLE_EH=True \
-	-DLLVM_ENABLE_RTTI=True \
-	-DLLVM_ENABLE_LIBCXX=True \
-	-DLLVM_ENABLE_LIBXML2=True \
-	-DLLVM_TOOL_LTO_BUILD=False \
-	-DLIBUNWIND_USE_COMPILER_RT=True \
-	-DCOMPILER_RT_BUILD_BUILTINS=True \
-	-DCOMPILER_RT_BUILD_SANITIZERS=False \
-	-DCOMPILER_RT_BUILD_XRAY=False \
-	-DCOMPILER_RT_BUILD_LIBFUZZER=False \
-	-DCOMPILER_RT_BUILD_PROFILE=False \
-	-DCOMPILER_RT_DEFAULT_TARGET_ONLY=True \
-	-DLIBCXXABI_USE_COMPILER_RT=True \
-	-DLIBCXXABI_USE_LLVM_UNWINDER=True \
-	-DLIBCXXABI_ENABLE_STATIC_UNWINDER=True \
-	-DLIBCXX_HAS_MUSL_LIBC=True \
-	-DLIBCXX_HAS_GCC_S_LIB=False \
-	-DLIBCXX_USE_COMPILER_RT=True \
-	-DLLVM_TABLEGEN=$PWD/host/bin/llvm-tblgen \
-	-DCLANG_TABLEGEN=$PWD/host/bin/clang-tblgen \
-	$PWD/src/$LLVM"
-
-#	-DCMAKE_CROSSCOMPILING=True \
-#  make -j12 -C build-stage-llvm
-#  make -C build-stage-llvm install
-}
-
-
 clean() {
-  rm -rf $PWD/build-prestage-*
   rm -rf $PWD/stage
   rm -rf $PWD/build-stage-*
-  rm -rf $PWD/host
-  rm -rf $PWD/build-host-*
 }
 
 
@@ -253,7 +139,6 @@ distclean() {
 
 all() {
   musl
-  libxml2
   libcxx
 }
 
