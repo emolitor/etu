a0c8d57d21fb401eb87f765f8ef20df914747554
Eric Molitor <eric@molitor.org>
2018-05-06 15:51:50 +0100

    Start working on 0.10 by breaking everything

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -43,8 +43,13 @@ init() {
   if [ ! -d $PWD/src ]; then
     mkdir -p $PWD/src
 
+    sh -c "cd src; git clone https://github.com/richfelker/musl-cross-make"
+    echo "TARGET=x86_64-linux-musl" > $PWD/src/musl-cross-make/config.mak
+    echo "OUTPUT=$PWD/host" >> $PWD/src/musl-cross-make/config.mak
+    echo "COMMON_CONFIG+=CFLAGS=\"-g0 -Os\" CXXFLAGS=\"-g0 -Os\" LDFLAGS=\"-s\"" >> $PWD/src/musl-cross-make/config.mak
+
     sh -c "cd src; curl -L -O $MUSL_URL/$MUSL_FILE"
-    sh -c "cd src; curl -L -O $LIBICU_URL/$LIBICU_FILE"
+#    sh -c "cd src; curl -L -O $LIBICU_URL/$LIBICU_FILE"
     sh -c "cd src; curl -L -O $LIBXML2_URL/$LIBXML2_FILE"
     sh -c "cd src; curl -L -O $LLVM_URL/$LLVM_FILE"
     sh -c "cd src; curl -L -O $LLVM_URL/$LIBUNWIND_FILE"
@@ -56,59 +61,100 @@ init() {
 
     sh -c "cd src; bsdtar -xf $MUSL_FILE"
 
-    mkdir -p src/$LIBICU
-    bsdtar -xf src/$LIBICU_FILE -C src/$LIBICU --strip 1
+#    mkdir -p src/$LIBICU
+#    bsdtar -xf src/$LIBICU_FILE -C src/$LIBICU --strip 1
 
     sh -c "cd src; bsdtar -xf $LIBXML2_FILE"
 
+    mkdir -p src/$LIBUNWIND
+    bsdtar -xf src/$LIBUNWIND_FILE -C src/$LIBUNWIND --strip 1
+    mkdir -p src/$LIBCXXABI
+    bsdtar -xf src/$LIBCXXABI_FILE -C src/$LIBCXXABI --strip 1
+    mkdir -p src/$LIBCXX
+    bsdtar -xf src/$LIBCXX_FILE -C src/$LIBCXX --strip 1
+
     mkdir -p src/$LLVM
     bsdtar -xf src/$LLVM_FILE -C src/$LLVM --strip 1
-    mkdir -p src/$LLVM/projects/libunwind
-    bsdtar -xf src/$LIBUNWIND_FILE -C src/$LLVM/projects/libunwind --strip 1
-    mkdir -p src/$LLVM/projects/libcxxabi
-    bsdtar -xf src/$LIBCXXABI_FILE -C src/$LLVM/projects/libcxxabi --strip 1
-    mkdir -p src/$LLVM/projects/libcxx
-    bsdtar -xf src/$LIBCXX_FILE -C src/$LLVM/projects/libcxx --strip 1
+    #mkdir -p src/$LLVM/projects/libunwind
+    #bsdtar -xf src/$LIBUNWIND_FILE -C src/$LLVM/projects/libunwind --strip 1
+    #mkdir -p src/$LLVM/projects/libcxxabi
+    #bsdtar -xf src/$LIBCXXABI_FILE -C src/$LLVM/projects/libcxxabi --strip 1
+    #mkdir -p src/$LLVM/projects/libcxx
+    #bsdtar -xf src/$LIBCXX_FILE -C src/$LLVM/projects/libcxx --strip 1
     mkdir -p src/$LLVM/projects/compiler-rt
     bsdtar -xf src/$COMPILER_RT_FILE -C src/$LLVM/projects/compiler-rt --strip 1
     mkdir -p src/$LLVM/tools/clang
     bsdtar -xf src/$CFE_FILE -C src/$LLVM/tools/clang --strip 1
-    mkdir -p src/$LLVM/tools/lld
-    bsdtar -xf src/$LLD_FILE -C src/$LLVM/tools/lld --strip 1
+    #mkdir -p src/$LLVM/tools/lld
+    #bsdtar -xf src/$LLD_FILE -C src/$LLVM/tools/lld --strip 1
   fi
 }
 
-#	-DCMAKE_CXX_FLAGS=\"--target=$TARGET -I$PWD/src/$LLVM/projects/libcxx/include\" \
+
+host_gcc() {
+    make -j12 -C $PWD/src/musl-cross-make
+    make -C $PWD/src/musl-cross-make install
+}
+
 
 sysroot() {
   init 
 
-  mkdir -p build-sysroot-musl
-  sh -c "cd build-sysroot-musl; \
-	CC=clang \
-	$PWD/src/$MUSL/configure \
-	--prefix=/"
-  make -j8 -C build-sysroot-musl
-  DESTDIR=$PWD/sysroot make -C build-sysroot-musl install 
+#  mkdir -p build-sysroot-musl
+#  sh -c "cd build-sysroot-musl; \
+#	CC=clang \
+#	$PWD/src/$MUSL/configure \
+#	--prefix=/"
+#  make -j8 -C build-sysroot-musl
+#  DESTDIR=$PWD/sysroot make -C build-sysroot-musl install 
+#	CC=clang CFLAGS=\"--sysroot=$PWD/sysroot\" \
+#	CXX=clang++ \
+#	cmake \
+
+  mkdir build-sysroot-libunwind
+  sh -c "cd build-sysroot-libunwind; cmake \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=$PWD/sysroot \
+	-DCMAKE_C_COMPILER=clang \
+	-DCMAKE_C_FLAGS=--sysroot=$PWD/sysroot \
+	-DCMAKE_CXX_COMPILER=clang++ \
+	-DCMAKE_INSTALL_PREFIX=$PWD/sysroot \
+	-DLLVM_PATH=$PWD/src/$LLVM \
+	$PWD/src/$LIBUNWIND"
+
+#	-DLIBUNWIND_ENABLE_SHARED=False \
+
+  make -j8 -C build-sysroot-libunwind unwind 
+
+  mkdir build-sysroot-libcxxabi
+  sh -c "cd build-sysroot-libcxxabi; cmake \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=$PWD/sysroot \
+	-DCMAKE_C_COMPILER=clang \
+	-DCMAKE_C_FLAGS=--sysroot=$PWD/sysroot \
+	-DCMAKE_CXX_COMPILER=clang++ \
+	-DCMAKE_SHARED_LINKER_FLAGS=\"-L$PWD/sysroot/lib -L$PWD/build-sysroot-libunwind/lib\" \
+	-DLIBCXXABI_USE_LLVM_UNWINDER=True \
+	-DLIBCXXABI_ENABLE_STATIC_UNWINDER=True \
+	-DLIBCXXABI_LIBUNWIND_PATH=$PWD/src/$LIBUNWIND \
+	-DLIBCXXABI_LIBCXX_INCLUDES=$PWD/src/$LIBCXX/include \
+	$PWD/src/$LIBCXXABI"
+  make -j8 -C build-sysroot-libcxx cxxabi 
 
   mkdir build-sysroot-libcxx
   sh -c "cd build-sysroot-libcxx; cmake \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX=$PWD/sysroot \
 	-DCMAKE_C_COMPILER=clang \
+	-DCMAKE_C_FLAGS=--sysroot=$PWD/sysroot \
 	-DCMAKE_CXX_COMPILER=clang++ \
-	-DCMAKE_C_FLAGS=\"--target=$TARGET \" \
-	-DCMAKE_CXX_FLAGS=\"--target=$TARGET -I$PWD/src/$LLVM/projects/libcxx/include\" \
-	-DCMAKE_SYSROOT=$PWD/sysroot \
-	-DLIBCXXABI_USE_LLVM_UNWINDER=True \
+	-DCMAKE_SHARED_LINKER_FLAGS=\"-L$PWD/sysroot/lib -L$PWD/build-sysroot-libcxxabi/lib\" \
 	-DLIBCXX_CXX_ABI=libcxxabi \
-	-DLIBCXX_CXX_ABI_INCLUDE_PATHS=$PWD/src/$LLVM/projects/libcxxabi/include \
+	-DLIBCXX_CXX_ABI_INCLUDE_PATHS=$PWD/src/$LIBCXXABI/include \
 	-DLIBCXX_HAS_MUSL_LIBC=True \
 	-DLIBCXX_HAS_GCC_S_LIB=False \
-	$PWD/src/$LLVM"
-
-  make -j8 -C build-sysroot-libcxx unwind install-unwind
-  make -j8 -C build-sysroot-libcxx cxxabi install-cxxabi
+	$PWD/src/$LIBCXX"
+exit
   make -j8 -C build-sysroot-libcxx cxx install-cxx
 }
 
