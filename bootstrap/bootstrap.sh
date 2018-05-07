bc2bd2357adec6be8fb6aba6b80449b2fd068391
Eric Molitor <eric@molitor.org>
2018-05-07 11:40:27 +0000

    WIP

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -6,7 +6,7 @@ LIBXML2_VERSION=2.9.8
 LLVM_VERSION=6.0.0
 
 # You shouldn't have to change anything below here
-TARGET=x86_64-pc-linux-musl
+TARGET=x86_64-linux-musl
 
 MUSL=musl-$MUSL_VERSION
 MUSL_FILE=$MUSL.tar.gz
@@ -51,7 +51,7 @@ init() {
 
     sh -c "cd src; curl -L -O $MUSL_URL/$MUSL_FILE"
 #    sh -c "cd src; curl -L -O $LIBICU_URL/$LIBICU_FILE"
-    sh -c "cd src; curl -L -O $LIBXML2_URL/$LIBXML2_FILE"
+#    sh -c "cd src; curl -L -O $LIBXML2_URL/$LIBXML2_FILE"
     sh -c "cd src; curl -L -O $LLVM_URL/$LLVM_FILE"
     sh -c "cd src; curl -L -O $LLVM_URL/$LIBUNWIND_FILE"
     sh -c "cd src; curl -L -O $LLVM_URL/$LIBCXXABI_FILE"
@@ -65,29 +65,29 @@ init() {
 #    mkdir -p src/$LIBICU
 #    bsdtar -xf src/$LIBICU_FILE -C src/$LIBICU --strip 1
 
-    sh -c "cd src; bsdtar -xf $LIBXML2_FILE"
+#    sh -c "cd src; bsdtar -xf $LIBXML2_FILE"
 
-    mkdir -p src/$LIBUNWIND
-    bsdtar -xf src/$LIBUNWIND_FILE -C src/$LIBUNWIND --strip 1
-    mkdir -p src/$LIBCXXABI
-    bsdtar -xf src/$LIBCXXABI_FILE -C src/$LIBCXXABI --strip 1
-    mkdir -p src/$LIBCXX
-    bsdtar -xf src/$LIBCXX_FILE -C src/$LIBCXX --strip 1
+    #mkdir -p src/$LIBUNWIND
+    #bsdtar -xf src/$LIBUNWIND_FILE -C src/$LIBUNWIND --strip 1
+    #mkdir -p src/$LIBCXXABI
+    #bsdtar -xf src/$LIBCXXABI_FILE -C src/$LIBCXXABI --strip 1
+    #mkdir -p src/$LIBCXX
+    #bsdtar -xf src/$LIBCXX_FILE -C src/$LIBCXX --strip 1
 
     mkdir -p src/$LLVM
     bsdtar -xf src/$LLVM_FILE -C src/$LLVM --strip 1
-    #mkdir -p src/$LLVM/projects/libunwind
-    #bsdtar -xf src/$LIBUNWIND_FILE -C src/$LLVM/projects/libunwind --strip 1
-    #mkdir -p src/$LLVM/projects/libcxxabi
-    #bsdtar -xf src/$LIBCXXABI_FILE -C src/$LLVM/projects/libcxxabi --strip 1
-    #mkdir -p src/$LLVM/projects/libcxx
-    #bsdtar -xf src/$LIBCXX_FILE -C src/$LLVM/projects/libcxx --strip 1
+    mkdir -p src/$LLVM/projects/libunwind
+    bsdtar -xf src/$LIBUNWIND_FILE -C src/$LLVM/projects/libunwind --strip 1
+    mkdir -p src/$LLVM/projects/libcxxabi
+    bsdtar -xf src/$LIBCXXABI_FILE -C src/$LLVM/projects/libcxxabi --strip 1
+    mkdir -p src/$LLVM/projects/libcxx
+    bsdtar -xf src/$LIBCXX_FILE -C src/$LLVM/projects/libcxx --strip 1
     mkdir -p src/$LLVM/projects/compiler-rt
     bsdtar -xf src/$COMPILER_RT_FILE -C src/$LLVM/projects/compiler-rt --strip 1
     mkdir -p src/$LLVM/tools/clang
     bsdtar -xf src/$CFE_FILE -C src/$LLVM/tools/clang --strip 1
-    #mkdir -p src/$LLVM/tools/lld
-    #bsdtar -xf src/$LLD_FILE -C src/$LLVM/tools/lld --strip 1
+    mkdir -p src/$LLVM/tools/lld
+    bsdtar -xf src/$LLD_FILE -C src/$LLVM/tools/lld --strip 1
   fi
 }
 
@@ -108,16 +108,63 @@ host_clang() {
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX=$PWD/host \
 	-DDEFAULT_SYSROOT=$PWD/sysroot \
-	-DC_INCLUDE_DIRS=/include \
 	-DCLANG_DEFAULT_CXX_STDLIB=libc++ \
 	-DCLANG_DEFAULT_LINKER=lld \
 	-DCLANG_DEFAULT_RTLIB=compiler-rt \
+	-DCOMPILER_RT_BUILD_SANITIZERS=False \
+	-DCOMPILER_RT_BUILD_XRAY=False \
+	-DCOMPILER_RT_BUILD_LIBFUZZER=False \
+	-DLIBCXXABI_USE_LLVM_UNWINDER=True \
+	-DLIBCXX_CXX_ABI=libcxxabi \
+	-DLIBCXX_CXX_ABI_INCLUDE_PATHS=$PWD/src/$LLVM/projects/libcxxabi/include \
+	-DLIBCXX_HAS_MUSL_LIBC=True \
+	-DLIBCXX_HAS_GCC_S_LIB=False \
 	-DLLVM_DEFAULT_TARGET_TRIPLE=$TARGET \
+	-DLLVM_ENABLE_EH=True \
+	-DLLVM_ENABLE_RTTI=True \
 	$PWD/src/$LLVM"
 }
 
-
 sysroot() {
+  init 
+
+  export PATH=$PATH:$PWD/host/bin
+
+  mkdir build-sysroot-libcxx
+  sh -c "cd build-sysroot-libcxx; cmake \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=$PWD/sysroot/usr \
+	-DCMAKE_SYSTEM_NAME=Linux \
+	-DCMAKE_C_COMPILER=x86_64-linux-musl-gcc \
+	-DCMAKE_CXX_COMPILER=x86_64-linux-musl-g++ \
+	-DCMAKE_SHARED_LINKER_FLAGS=-L$PWD/build-sysroot-libcxx/lib \
+	-DCOMPILER_RT_BUILD_SANITIZERS=False \
+	-DCOMPILER_RT_BUILD_XRAY=False \
+	-DCOMPILER_RT_BUILD_LIBFUZZER=False \
+	-DLIBCXXABI_USE_LLVM_UNWINDER=True \
+	-DLIBCXX_CXX_ABI=libcxxabi \
+	-DLIBCXX_CXX_ABI_INCLUDE_PATHS=$PWD/src/$LLVM/projects/libcxxabi/include \
+	-DLIBCXX_HAS_MUSL_LIBC=True \
+	-DLIBCXX_HAS_GCC_S_LIB=False \
+	-DLLVM_ENABLE_EH=True \
+	-DLLVM_ENABLE_RTTI=True \
+	$PWD/src/$LLVM"
+
+  make -j8 -C build-sysroot-libcxx compiler-rt install-compiler-rt
+  make -j8 -C build-sysroot-libcxx unwind install-unwind
+  make -j8 -C build-sysroot-libcxx cxxabi install-cxxabi
+  make -j8 -C build-sysroot-libcxx cxx install-cxx
+
+  mkdir -p build-sysroot-musl
+  sh -c "cd build-sysroot-musl; \
+	CC=clang \
+	$PWD/src/$MUSL/configure \
+	--prefix=/usr"
+  make -j8 -C build-sysroot-musl
+  DESTDIR=$PWD/sysroot make -C build-sysroot-musl install 
+}
+
+sysroot_old() {
   init 
 
   export PATH=$PATH:$PWD/host/bin
@@ -129,7 +176,8 @@ sysroot() {
 	-DCMAKE_SYSTEM_NAME=Linux \
 	-DCMAKE_C_COMPILER=x86_64-linux-musl-gcc \
 	-DCMAKE_CXX_COMPILER=x86_64-linux-musl-g++ \
-	-DLIBUNWIND_USE_COMPILER_RT=True \
+	-DLLVM_ENABLE_EH=True \
+	-DLLVM_ENABLE_RTTI=True \
 	$PWD/src/$LIBUNWIND"
 
   make -j8 -C build-sysroot-libunwind unwind install-unwind
@@ -141,11 +189,12 @@ sysroot() {
 	-DCMAKE_SYSTEM_NAME=Linux \
 	-DCMAKE_C_COMPILER=x86_64-linux-musl-gcc \
 	-DCMAKE_CXX_COMPILER=x86_64-linux-musl-g++ \
-	-DCMAKE_SHARED_LINKER_FLAGS=-L$PWD/sysroot/lib \
-	-DLIBCXXABI_USE_COMPILER_RT=True \
+	-DCMAKE_SHARED_LINKER_FLAGS=-L$PWD/sysroot/usr/lib \
 	-DLIBCXXABI_USE_LLVM_UNWINDER=True \
 	-DLIBCXXABI_LIBUNWIND_PATH=$PWD/src/$LIBUNWIND \
 	-DLIBCXXABI_LIBCXX_INCLUDES=$PWD/src/$LIBCXX/include \
+	-DLLVM_ENABLE_EH=True \
+	-DLLVM_ENABLE_RTTI=True \
 	$PWD/src/$LIBCXXABI"
 
   make -j8 -C build-sysroot-libcxxabi cxxabi install-cxxabi
@@ -157,13 +206,14 @@ sysroot() {
 	-DCMAKE_SYSTEM_NAME=Linux \
 	-DCMAKE_C_COMPILER=x86_64-linux-musl-gcc \
 	-DCMAKE_CXX_COMPILER=x86_64-linux-musl-g++ \
-	-DCMAKE_SHARED_LINKER_FLAGS=-L$PWD/sysroot/lib \
+	-DCMAKE_SHARED_LINKER_FLAGS=-L$PWD/sysroot/usr/lib \
 	-DLIBCXXABI_USE_LLVM_UNWINDER=True \
-	-DLIBCXX_USE_COMPILER_RT=True \
 	-DLIBCXX_CXX_ABI=libcxxabi \
 	-DLIBCXX_CXX_ABI_INCLUDE_PATHS=$PWD/src/$LIBCXXABI/include \
 	-DLIBCXX_HAS_MUSL_LIBC=True \
 	-DLIBCXX_HAS_GCC_S_LIB=False \
+	-DLLVM_ENABLE_EH=True \
+	-DLLVM_ENABLE_RTTI=True \
 	$PWD/src/$LIBCXX"
 
   make -j8 -C build-sysroot-libcxx cxx install-cxx
